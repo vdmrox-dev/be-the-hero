ad3093f720c3d067cd7f88c6b19f101c62660615
arctouch-viniciusmoraes <vinicius.moraes@quizlet.com>
2020-03-29 19:47:52 -0300

    implement backend

---
+++ backend/src/controllers/IncidentsController.js
@@ -0,0 +1,59 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(request, response) {
+    const { page = 1 } = request.query;
+
+    const [count] = await connection('incidents').count();
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .offset((page - 1) * 5)
+      .limit(5)
+      .select([
+        'incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.state'
+      ]);
+
+    response.header('X-Total-Count', count['count(*)']);
+
+    return response.json(incidents);
+  },
+
+  async create(request, response) {
+    const { title, description, value } = request.body;
+    const ong_Id = request.headers.authorization;
+
+    const [id] = await connection('incidents').insert({
+      title,
+      description,
+      value,
+      ong_Id
+    });
+
+    return response.json({ id });
+  },
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
+
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
+
+    if (ong_id !== incident.ong_id) {
+      return response.status(401).json({ error: 'Operation not authorized' });
+    }
+
+    await connection('incidents')
+      .where('id', id)
+      .delete();
+
+    return response.status(204).send();
+  }
+};
